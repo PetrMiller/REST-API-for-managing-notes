0585d3b3965a1121f7ce67ad9c7a383a6f117676
PetrMiller <petr.kochetov0410@gmail.com>
2024-10-03 14:11:16 +0700

    delete config.py and manage.py

---
+++ README.md
@@ -0,0 +1,8 @@
+1)launching Docker's containers  
+docker-compose up
+
+2) check running containers
+docker ps
+
+3)launching the app
+py main.py
--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from config import Config
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 db = SQLAlchemy()
-migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['DEBUG'] = os.getenv('DEBUG', 'False') == 'True'
 
     db.init_app(app)
-    migrate.init_app(app, db)
 
     from .routes import note_bp
     app.register_blueprint(note_bp)
